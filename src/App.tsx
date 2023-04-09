91b1fca2a99b6cb7962370c32e11f8cd8eb7f0dd
Tobías Pasinato <123979610+tobiaspasinato@users.noreply.github.com>
2023-04-09 20:06:44 -0300

    Areglo chico a pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './Functions/table'
 import BasicExample from './Functions/table';
 import NavBar from './Functions/NavBar'
 import TablaDeTipos from './Pages/TablaDeTipos';
-import Pokedex from './Pages/pokedex';
+import Pokedex from './Pages/Pokedex';
 import Home from './Pages/Home';
 import { Route, Routes} from "react-router-dom"
 
